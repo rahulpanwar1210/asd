b8d268a0838750c7429173ef6de11ee4341a265b
rahulpanwar1210 <43522504+rahulpanwar1210@users.noreply.github.com>
2018-09-27 00:08:48 +0530

    Update pythoncodeformnist(basicNN).py

--- pythoncodeformnist(basicNN).py
+++ pythoncodeformnist(basicNN).py
@@ -64,10 +64,10 @@ def baseline_model():
 # build the model
 model = baseline_model()
 # Fit the model
-model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=10, batch_size=200, verbose=2)
+#model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=10, batch_size=200, verbose=2)
 # Final evaluation of the model
-scores = model.evaluate(X_test, y_test, verbose=0)
-print("Baseline Error: %.2f%%" % (100-scores[1]*100))
+#scores = model.evaluate(X_test, y_test, verbose=0)
+#print("Baseline Error: %.2f%%" % (100-scores[1]*100))
 
 
 
